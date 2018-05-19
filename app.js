2855cc9a264b973285034f85e53e0a084277a2d0
andiblake <andibclark@outlook.com>
2018-05-18 19:34:49 -0500

    k

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var path = require('path');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/world.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
 
-app.get('/ind', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
+app.get('/ind', (req, res) => res.sendFile(path.join(__dirname + '/public/world.html')));
 
 
 app.listen(8080, () => console.log('Your site listening on port 8080!'));
